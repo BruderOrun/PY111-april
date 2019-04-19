11d114e987abc7903ce789cc4193d75f6e372774
Nikita Orunov <norunov@stream.ru>
2019-04-19 17:04:40 +0300

    lib_0

---
+++ Lib_book/lib_book.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+def choice():
+    subprocess.call(['ls', '/Users/norunov/Documents/python/politech/PY111-april/the_choice' ])
+    name_book_choice = input('Скопируйте и вставьте в терминал имя книги. Для добавления в библиотеку')
+
+
+
+g = [[1,'name','disk'],['name2','disk2']]
+
+# with open(oldfile, 'r', encoding='utf-8') as infile:
+def ds():
+    for i in g:
+
+     print(g[1])
